4e8c822d7ef994002d807a5686252a364058643e
Carg21 <carlosgv512@hotmail.com>
2025-03-22 10:29:01 +0000

    nuevo

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+# hay 6 errores segun el test pero no los encuentro.
 """
 import os
 from flask import Flask, request, jsonify, url_for
@@ -12,33 +13,62 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+  
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods =['GET'])
+def get_single_member(id):
+    
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+   
+    return jsonify({"error": "Miembro en la familia no encontrado"}), 404
+   
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member', methods =['POST'])
+def add_miembro():
+    member = request.json
+    if not member:
+        return jsonify({"error": "No se pudo añadir ningun mienbro"}), 400
+    
+    jackson_family.add_member(member)
+    
+    return jsonify({"mensaje": "Un nuevo miembro añadido a la familia"}), 200
+    
+  
+ 
+@app.route('/member/<int:id>', methods =['DELETE'])
+def delete_single_member(id):
+   
+    member =jackson_family.get_member(id)
+    
+    if member:
+        jackson_family.delete_member(id)
+
+        return jsonify({"mensaje": "Miembro eliminado correctamente"}), 200
+    
+    return jsonify({"error": "No se pudo eliminar el miembro"}), 400
+   
+   
+        
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
